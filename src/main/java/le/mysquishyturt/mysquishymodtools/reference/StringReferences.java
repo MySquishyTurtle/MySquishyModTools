9a65cecb4ece196d9c4e8ff6ba38f18c2ee52db1
MySquishyTurtle <MySquishyTurtle@gmail.com>
2016-03-24 20:32:05 -0400

    Update version

--- src/main/java/le/mysquishyturt/mysquishymodtools/reference/StringReferences.java
+++ src/main/java/le/mysquishyturt/mysquishymodtools/reference/StringReferences.java
@@ -4,7 +4,7 @@ public class StringReferences {
 
     public static final String MODID = "mysquishymodtools";
     public static final String NAME = "MySquishyModTools";
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.1.2";
     public static final String clientSide = "le.mysquishyturt.mysquishymodtools.proxies.ClientProxy";
     public static final String serverSide = "le.mysquishyturt.mysquishymodtools.proxies.ServerProxy";
     public static final String modEnabled = "MySquishyModTools enabled.";
